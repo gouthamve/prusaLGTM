5a0bd39f22d8c94bb67e7477a4fb1d53f83de7bf
Goutham <gouthamve@gmail.com>
2024-07-07 09:04:16 +0200

    Timelapse generator
    Signed-off-by: Goutham <gouthamve@gmail.com>
    Signed-off-by: gouthamve <gouthamve@gmail.com>

--- cli/prusaLGTM.go
+++ cli/prusaLGTM.go
@@ -1,9 +1,9 @@
 package cli
 
 var PrusaLGTM struct {
-	PrintImage printImage `cmd:"print-image" help:"Print images from a camera to stdout."`
-
-	FailureDetect failureDetectCommand `cmd:"failure-detect" help:"Detect failures in the print images."`
+	PrintImage        printImage               `cmd:"print-image" help:"Print images from a camera to stdout."`
+	FailureDetect     failureDetectCommand     `cmd:"failure-detect" help:"Detect failures in the print images."`
+	GenerateTimelapse generateTimelapseCommand `cmd:"generate-timelapse" help:"Generate a timelapse video from the print images."`
 
 	PrometheusPort int `kong:"help='The port to expose Prometheus metrics on.',default='8366',name='prometheus-port'"`
 }
